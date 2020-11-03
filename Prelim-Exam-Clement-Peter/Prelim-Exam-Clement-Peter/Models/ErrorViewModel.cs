a5b374739ce7a6138a31a63a6e9e388f26ea2771
Peter Paul.Clemente <72077161+Peterpaulclemente@users.noreply.github.com>
2020-11-03 12:09:29 +0800

    Commit a message

---
+++ Prelim-Exam-Clement-Peter/Prelim-Exam-Clement-Peter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Prelim_Exam_Clement_Peter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
